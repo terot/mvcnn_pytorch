db0818dcff28d86218550bbd8c6068bfe9b0e057
Tero Tapiovaara <tero.tapiovaara@silvere.com>
2019-05-15 13:38:39 +0300

    init_from parameter

--- train_mvcnn.py
+++ train_mvcnn.py
@@ -24,6 +24,7 @@ parser.add_argument("-n_classes", type=int, help="number of classes", default=40
 parser.add_argument("-n_epochs", type=int, help="number of epochs", default=30)
 parser.add_argument("-classnames", type=str, help="class names", default=None)
 parser.add_argument("-no_cuda", dest='no_cuda', action='store_true')
+parser.add_argument("-init_from", type=str, help="init with pretrained model", default=None)
 
 parser.set_defaults(train=False)
 
@@ -36,53 +37,80 @@ def create_folder(log_dir):
         shutil.rmtree(log_dir)
         os.mkdir(log_dir)
 
-if __name__ == '__main__':
-    args = parser.parse_args()
-
-    classnames = args.classnames.split(',')
-
-    pretraining = not args.no_pretraining
-    log_dir = args.name
-    create_folder(args.name)
-    config_f = open(os.path.join(log_dir, 'config.json'), 'w')
-    json.dump(vars(args), config_f)
-    config_f.close()
-
-    # STAGE 1
+def train_stage1(args):
     log_dir = args.name+'_stage_1'
+    pretraining = not args.no_pretraining
+    classnames = args.classnames.split(',')
+    n_models_train = args.num_models*args.num_views
+
     create_folder(log_dir)
-    cnet = SVCNN(args.name, nclasses=args.n_classes, pretraining=pretraining, cnn_name=args.cnn_name, no_cuda=args.no_cuda)
+    cnet = SVCNN(args.name, nclasses=args.n_classes, pretraining=pretraining,
+                 cnn_name=args.cnn_name, no_cuda=args.no_cuda)
 
     optimizer = optim.Adam(cnet.parameters(), lr=args.lr, weight_decay=args.weight_decay)
     
-    n_models_train = args.num_models*args.num_views
 
     print("train_path: {}".format(args.train_path))
-    train_dataset = SingleImgDataset(args.train_path, scale_aug=False, rot_aug=False, num_models=n_models_train, num_views=args.num_views, classnames=classnames)
+    train_dataset = SingleImgDataset(args.train_path, scale_aug=False, rot_aug=False,
+                                     num_models=n_models_train, num_views=args.num_views,
+                                     classnames=classnames)
     print("train_dataset: {}".format(len(train_dataset)))
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True, num_workers=0)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=64, shuffle=True,
+                                               num_workers=0)
 
-    val_dataset = SingleImgDataset(args.val_path, scale_aug=False, rot_aug=False, test_mode=True, classnames=classnames)
+    val_dataset = SingleImgDataset(args.val_path, scale_aug=False, rot_aug=False,
+                                   test_mode=True, classnames=classnames)
     val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=False, num_workers=0)
     print('num_train_files: '+str(len(train_dataset.filepaths)))
     print('num_val_files: '+str(len(val_dataset.filepaths)))
     trainer = ModelNetTrainer(cnet, train_loader, val_loader, optimizer, nn.CrossEntropyLoss(), 'svcnn', log_dir, num_views=1, n_classes=args.n_classes, no_cuda=args.no_cuda)
     trainer.train(args.n_epochs)
 
-    # STAGE 2
+    return cnet
+
+def train_stage2(cnet, args):
     log_dir = args.name+'_stage_2'
+    classnames = args.classnames.split(',')
+    n_models_train = args.num_models*args.num_views
+
     create_folder(log_dir)
-    cnet_2 = MVCNN(args.name, cnet, nclasses=args.n_classes, cnn_name=args.cnn_name, num_views=args.num_views, no_cuda=args.no_cuda)
-    del cnet
+    if args.init_from is None:
+        cnet_2 = MVCNN(args.name, cnet, nclasses=args.n_classes, cnn_name=args.cnn_name,
+                       num_views=args.num_views, no_cuda=args.no_cuda)
+        del cnet
+    else:
+        cnet_2 = torch.load(args.init_from)
 
-    optimizer = optim.Adam(cnet_2.parameters(), lr=args.lr, weight_decay=args.weight_decay, betas=(0.9, 0.999))
+    optimizer = optim.Adam(cnet_2.parameters(), lr=args.lr, weight_decay=args.weight_decay,
+                           betas=(0.9, 0.999))
     
-    train_dataset = MultiviewImgDataset(args.train_path, scale_aug=False, rot_aug=False, num_models=n_models_train, num_views=args.num_views, classnames=classnames)
-    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batchSize, shuffle=False, num_workers=0)# shuffle needs to be false! it's done within the trainer
+    train_dataset = MultiviewImgDataset(args.train_path, scale_aug=False, rot_aug=False,
+                                        num_models=n_models_train, num_views=args.num_views, classnames=classnames)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=args.batchSize,
+                                               shuffle=False, num_workers=0) # shuffle needs to be false! it's done within the trainer
 
-    val_dataset = MultiviewImgDataset(args.val_path, scale_aug=False, rot_aug=False, num_views=args.num_views, classnames=classnames)
-    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batchSize, shuffle=False, num_workers=0)
+    val_dataset = MultiviewImgDataset(args.val_path, scale_aug=False, rot_aug=False,
+                                      num_views=args.num_views, classnames=classnames)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=args.batchSize,
+                                             shuffle=False, num_workers=0)
     print('num_train_files: '+str(len(train_dataset.filepaths)))
     print('num_val_files: '+str(len(val_dataset.filepaths)))
-    trainer = ModelNetTrainer(cnet_2, train_loader, val_loader, optimizer, nn.CrossEntropyLoss(), 'mvcnn', log_dir, num_views=args.num_views, n_classes=args.n_classes, no_cuda=args.no_cuda)
+    trainer = ModelNetTrainer(cnet_2, train_loader, val_loader, optimizer, nn.CrossEntropyLoss(),
+                              'mvcnn', log_dir, num_views=args.num_views, n_classes=args.n_classes,
+                              no_cuda=args.no_cuda)
     trainer.train(args.n_epochs)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+
+    log_dir = args.name
+    create_folder(args.name)
+    config_f = open(os.path.join(log_dir, 'config.json'), 'w')
+    json.dump(vars(args), config_f)
+    config_f.close()
+
+    if args.init_from is None:
+        cnet = train_stage1(args)
+    else:
+        cnet = None
+    train_stage2(cnet, args)
